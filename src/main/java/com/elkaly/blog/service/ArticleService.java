68676ea90e0f28408cd7a8bc0fba062241b9391d
elkaly <elkaly@naver.com>
2015-01-18 23:06:11 +0900

    재생성

--- src/main/java/com/elkaly/blog/service/ArticleService.java
+++ src/main/java/com/elkaly/blog/service/ArticleService.java
@@ -5,12 +5,24 @@ import com.elkaly.blog.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class ArticleService {
 
-    @Autowired ArticleRepository articleRepository;
+
+    @Resource(name = "ArticleRepository")
+    private ArticleRepository articleRepository;
+
 
     public void postArticle(Article article){
-        articleRepository.postArticle(article);
+        System.out.println("CALL article service");
+
+        //articleRepository.postArticle(article);
+        articleRepository.selectArticleList();
+
     }
+
+
+
 }

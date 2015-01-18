c5ae90fab80e402880c3f27a0f5ebb2cdc80316b
elkaly <elkaly@naver.com>
2015-01-18 15:04:30 +0900

    sqlSession 수정

--- src/main/java/com/elkaly/blog/controller/ArticleController.java
+++ src/main/java/com/elkaly/blog/controller/ArticleController.java
@@ -1,10 +1,9 @@
 package com.elkaly.blog.controller;
 
+import com.elkaly.blog.model.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jeahyukkim on 15. 1. 14..
@@ -12,32 +11,48 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ArticleController {
 
+    //Article CRUD
+
     //POST
     @RequestMapping(method = RequestMethod.POST, value = "/article")
-    public String postAtricle(ModelMap model) {
-        model.addAttribute("message", "POST Method Call");
-        return "hello";
+    @ResponseBody
+    public Article postAtricle(@RequestBody Article article) {
+
+        return article;
     }
 
     //GET
     @RequestMapping(method = RequestMethod.GET, value = "/articles/{articleNo}")
+    @ResponseBody
     public String getAtricle(ModelMap model, @PathVariable("articleNo") Integer articleNo) {
         model.addAttribute("message", "GET Method Call");
-        return "hello";
+        return "article/article";
     }
 
     //PUT
     @RequestMapping(method = RequestMethod.PUT, value = "/articles/{articleNo}")
+    @ResponseBody
     public String putAtricle(ModelMap model, @PathVariable("articleNo") Integer articleNo) {
         model.addAttribute("message", "PUT Method Call");
-        return "hello";
+        return "article/article";
     }
 
     //DELETE
     @RequestMapping(method = RequestMethod.DELETE, value = "/articles/{articleNo}")
+    @ResponseBody
     public String deleteAtricle(ModelMap model, @PathVariable("articleNo") Integer articleNo) {
         model.addAttribute("message", "DELETE Method Call");
-        return "hello";
+        return "article/article";
+    }
+
+
+    //Article List
+    //GET
+    @RequestMapping(method = RequestMethod.GET, value = "/articles")
+    @ResponseBody
+    public String getAtricleList(ModelMap model) {
+        model.addAttribute("message", "GET Method Call");
+        return "article/articleList";
     }
 
 }

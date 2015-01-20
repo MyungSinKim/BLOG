2aa37c19a24c03b2ad7d94f04bd7c535d155de80
elkaly <elkaly@naver.com>
2015-01-20 18:31:46 +0900

    controller수정

--- src/main/java/com/elkaly/blog/controller/ArticleController.java
+++ src/main/java/com/elkaly/blog/controller/ArticleController.java
@@ -2,12 +2,17 @@ package com.elkaly.blog.controller;
 
 import com.elkaly.blog.model.Article;
 import com.elkaly.blog.service.ArticleService;
+import com.google.gson.Gson;
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jeahyukkim on 15. 1. 14..
@@ -18,47 +23,45 @@ public class ArticleController {
     @Autowired
     ArticleService articleService;
 
-    //Article CRUD
 
+
+
+    //Article CRUD
     //POST
     @RequestMapping(method = RequestMethod.POST, value = "/article")
-    @ResponseBody
-    public String postAtricle(@RequestBody Article article) {
+    public String postAtricle(ModelMap model, @RequestBody Article article) {
         articleService.postArticle(article);
-        return getAtricle(article.getArticleNo()); //getArticle
+        return getAtricle(model, article.getArticleNo()); //getArticle
     }
 
     //GET
     @RequestMapping(method = RequestMethod.GET, value = "/articles/{articleNo}")
-    @ResponseBody
-    public String getAtricle(@PathVariable("articleNo") Integer articleNo) {
+    public String getAtricle(ModelMap model, @PathVariable("articleNo") Integer articleNo) {
+        Article article = articleService.getArticle(articleNo);
+        model.addAttribute("article", article);
         return "article/article";
     }
 
     //PUT
     @RequestMapping(method = RequestMethod.PUT, value = "/articles/{articleNo}")
-    @ResponseBody
-    public String putAtricle(ModelMap model, @PathVariable("articleNo") Integer articleNo) {
-        model.addAttribute("message", "PUT Method Call");
-        return "article/article";
+    public String putArticle(ModelMap model, @PathVariable("articleNo") Integer articleNo, @RequestBody Article article) {
+        articleService.putArticle(article);
+        return getAtricle(model, article.getArticleNo());
     }
 
     //DELETE
     @RequestMapping(method = RequestMethod.DELETE, value = "/articles/{articleNo}")
-    @ResponseBody
     public String deleteAtricle(ModelMap model, @PathVariable("articleNo") Integer articleNo) {
-        model.addAttribute("message", "DELETE Method Call");
-        return "article/article";
+        articleService.deleteArticle(articleNo);
+        return getAtricleList(model);
     }
-
 
     //Article List
     //GET
     @RequestMapping(method = RequestMethod.GET, value = "/articles")
-    @ResponseBody
     public String getAtricleList(ModelMap model) {
-        model.addAttribute("message", "GET Method Call");
+        String json = new Gson().toJson(articleService.getArticles() );
+        model.addAttribute("articles", json);
         return "article/articleList";
     }
-
 }
---
+++ src/main/java/com/elkaly/blog/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.elkaly.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by jeahyukkim on 15. 1. 19..
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String index(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+
+        return "index";
+    }
+
+}

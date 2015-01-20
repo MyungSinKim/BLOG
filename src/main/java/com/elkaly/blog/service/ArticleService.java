d13b6f3a9d92b59afe4664bdc269f257840ae5f3
elkaly <elkaly@naver.com>
2015-01-20 18:32:22 +0900

    service 추가

--- src/main/java/com/elkaly/blog/service/ArticleService.java
+++ src/main/java/com/elkaly/blog/service/ArticleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class ArticleService {
@@ -15,12 +16,23 @@ public class ArticleService {
 
 
     public void postArticle(Article article){
-        System.out.println("CALL article service");
-
         articleRepository.postArticle(article);
-
     }
 
+    public Article getArticle(Integer articleNo){
+        return articleRepository.getArticle(articleNo);
+    }
 
+    public void putArticle(Article article){
+        articleRepository.putArticle(article);
+    }
+
+    public void deleteArticle(Integer articleNo){
+        articleRepository.deleteArticle(articleNo);
+    }
+
+    public List<Article> getArticles(){
+        return articleRepository.getArticles();
+    }
 
 }

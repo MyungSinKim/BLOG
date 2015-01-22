55efde592dfb9913bd336cde6e87bdacc6404054
elkaly <elkaly@naver.com>
2015-01-23 02:54:54 +0900

    모델생성

--- src/main/java/com/elkaly/blog/model/ArticleList.java
+++ src/main/java/com/elkaly/blog/model/ArticleList.java
@@ -4,7 +4,35 @@ import java.util.List;
 
 public class ArticleList {
 
+    private Integer total;
+    private Integer page;
+    private Integer records;
     private List<Article> rows;
+
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRecords() {
+        return records;
+    }
+
+    public void setRecords(Integer records) {
+        this.records = records;
+    }
 
     public List<Article> getRows() {
         return rows;
@@ -14,10 +42,11 @@ public class ArticleList {
         this.rows = rows;
     }
 
-
-
     //Constructor
-    public ArticleList(List<Article> rows){
+    public ArticleList(Integer total, Integer page, Integer records,  List<Article> rows){
+        this.total = total;
+        this.page = page;
+        this.records = records;
         this.rows = rows;
     }
 }
